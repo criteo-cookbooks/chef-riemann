db679f672aa2e3e547701ad44e196ed3e6a38b63
Maximilian Rüdiger <m.ruediger@alice-dsl.net>
2013-03-16 22:21:48 +0100

    use defaults from attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default.set["riemann_server"]["version"] = '0.1.5'
+default["riemann_server"]["version"] = '0.1.5'
 default.set['java']['oracle']['accept_oracle_download_terms'] = true
 
--- recipes/server.rb
+++ recipes/server.rb
@@ -4,22 +4,12 @@ user "riemann" do
   system true
 end
 
-if Chef::Config[:solo]
-  riemann_server = {
-    "riemann_server_ipaddress" => node.ipaddress.first,
-  }
-  databag_item = Chef::DataBagItem.new
-  databag_item.data_bag("riemann_server")
-  databag_item.raw_data = riemann_server
-  databag_item.save
-end
-
-remote_file "/tmp/riemann_#{version}_all.deb" do
-  source "http://aphyr.com/riemann/riemann_#{version}_all.deb"
+remote_file "/tmp/riemann_#{node.riemann_server.version}_all.deb" do
+  source "http://aphyr.com/riemann/riemann_#{node.riemann_server.version}_all.deb"
   mode 0644
 end
 
-dpkg_package "/tmp/riemann_#{version}_all.deb"
+dpkg_package "/tmp/riemann_#{node.riemann_server.version}_all.deb"
 
 runit_service "riemann"
 
