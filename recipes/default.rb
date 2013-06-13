c58bea0de703116f51fc1953ebb65d67d163f725
Stephan Renatus <s.renatus@cloudbau.de>
2013-06-13 12:29:17 +0200

    make rbenv non-global

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,4 @@ include_recipe 'runit'
 include_recipe 'rbenv::default'
 include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby node[:riemann][:ruby_version] do
-  global true
-end
+rbenv_ruby node[:riemann][:ruby_version]
